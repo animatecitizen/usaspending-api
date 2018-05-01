32bcbd565dff1fdb5e2f2b4e1d5bfd2c72d67473
Daniel Boos <boos_daniel@bah.com>
2018-05-01 10:00:29 -0400

    Allowing trailing slashes for recipient endpoints

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from usaspending_api.recipient.v2.views import recipient
 
 urlpatterns = [
-    url(r'^duns/(?P<duns>[\w\d]+)$', recipient.DUNSViewSet.as_view())
+    url(r'^duns/(?P<duns>[\w\d]+)/?$', recipient.DUNSViewSet.as_view())
     # Commented out until LEI is implemented
-    # url(r'^lei/(?P<lei>[0-9]+)$', recipient.LEIViewSet.as_view())
+    # url(r'^lei/(?P<lei>[0-9]+)/?$', recipient.LEIViewSet.as_view())
 ]
